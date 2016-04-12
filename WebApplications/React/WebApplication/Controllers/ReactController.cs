3ea0891977fba2630500d5faa64265b3755ee391
Roberto75 <roberto.rutigliano@gmail.com>
2016-04-12 18:11:01 +0200

    React

---
+++ WebApplications/React/WebApplication/Controllers/ReactController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace My.React.WebApplication.Controllers
+{
+    public class ReactController : Controller
+    {
+     
+        public ActionResult Index()
+        {
+            //http://templecoding.com/blog/2015/12/11/using-react-typescript-webpack-and-visual-studio/
+
+            //http://www.samulihaverinen.com/web-development/dotnet/2016/01/04/getting-started-with-react-mvc6-typescript/
+
+
+            //visual studio code react intellisense
+
+            return View();
+        }
+
+    }
+}
