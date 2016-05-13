0d4cef68cb2cc39ae24f9b84839335cbdc7326ea
Daitarn <roberto.rutigliano@gmail.com>
2016-05-13 16:30:30 +0200

    TEST sui caratteri unicode sul database

--- UnitTestProjectCSharp/SqlServer.cs
+++ UnitTestProjectCSharp/SqlServer.cs
@@ -10,32 +10,61 @@ namespace UnitTestProjectCSharp
     public class SqlServer
     {
         [TestMethod]
-        public void Sloveno()
+        public void Unicode()
         {
             string sqlQuery;
-            sqlQuery = "INSERT INTO NOTE ( NOTA ) VALUES ( @NOTA )";
 
+
+            //string notaNVARCHAR;
+            //string notaVARCHAR
             string nota;
-            
+
             ManagerDB manager = new ManagerDB("DefaultConnection");
             manager.mOpenConnection();
 
             try
             {
-                System.Data.Common.DbCommand command;
-                command = manager.mGetConnection().CreateCommand();
-                command.Connection = manager.mGetConnection();
+                //System.Data.Common.DbCommand command;
+                //command = manager.mGetConnection().CreateCommand();
+                //command.Connection = manager.mGetConnection();
 
-                nota = "Dejavnosti Googlovo brezplačno storitev takoj prevede besede, stavke in spletne strani med italijansko in več kot 100 drugih jezikih. Italijanski, Afrikaans, albanski, amharščina, arabščina, ..";
+
                 //nota = "CIAO città!";
-                manager.mAddParameter(command, "@NOTA", nota);
 
-                command.CommandText = sqlQuery;
-                command.CommandType = CommandType.Text;
+                //sqlQuery = "INSERT INTO NOTE ( NOTA , NOTA2 ) VALUES ( N@NOTA , N@NOTA2 )";
+                //manager.mAddParameter(command, "@NOTA", nota);
+                //manager.mAddParameter(command, "@NOTA2", nota);
 
-                manager.mExecuteNoQuery(command);
+                //command.CommandText = sqlQuery;
+                //command.CommandType = CommandType.Text;
+
+                //manager.mExecuteNoQuery(command);
 
 
+                //nota = "brezplačno jezikih.frikaans, albanski, amharščina, arabščina,  ..";
+                //sqlQuery = "INSERT INTO NOTE ( NOTA , NOTA2 ) VALUES ( '" + nota + "', '" + nota + "' )";
+                //manager.mExecuteNoQuery(sqlQuery);
+
+
+                //sqlQuery = "INSERT INTO NOTE ( NOTA , NOTA2 ) VALUES ( N'" + nota + "',  N'" + nota + "' )";
+                //manager.mExecuteNoQuery(sqlQuery);
+
+                //nota = "città perchè però più ";
+                //sqlQuery = "INSERT INTO NOTE ( NOTA , NOTA2 ) VALUES ( '" + nota + "', '" + nota + "' )";
+                //manager.mExecuteNoQuery(sqlQuery);
+
+                //sqlQuery = "INSERT INTO NOTE ( NOTA , NOTA2 ) VALUES ( N'" + nota + "', N'" + nota + "' )";
+                //manager.mExecuteNoQuery(sqlQuery);
+
+
+                nota = "الجنة هو اللث من ثلاثة أجزاء التي تالكوميديا ​​هية لدانتي أليغييري ، بعد الجحيم وم";
+                sqlQuery = "INSERT INTO NOTE ( NOTA , NOTA2 ) VALUES ( N'" + nota + "', N'" + nota + "' )";
+                //manager.mExecuteNoQuery(sqlQuery);
+
+
+                nota = "天堂是三部分构成由但丁神曲地狱炼狱之后的第三次。在他的书信十三但丁他献给诗灿大斯卡拉。";
+                sqlQuery = "INSERT INTO NOTE ( NOTA , NOTA2 ) VALUES ( N'" + nota + "', N'" + nota + "' )";
+                manager.mExecuteNoQuery(sqlQuery);
 
                 sqlQuery = "SELECT * FROM NOTE";
                 DataTable dt;
@@ -43,7 +72,8 @@ namespace UnitTestProjectCSharp
 
                 foreach (DataRow row  in dt.Rows)
                 {
-                    Debug.WriteLine(row["nota"]);
+                    Debug.WriteLine("NVARCHAR: " + row["nota"] + "  - VARCHAR: "+ row["nota2"]);
+                    //Debug.WriteLine("NOTA 2" + row["nota2"]);
                 }
                 
 
